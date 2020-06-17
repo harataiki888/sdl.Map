8f296e8265c75b8f7ffe9392695dc907bf9e1a89
harataiki888 <hara.t.aq@m.titech.ac.jp>
2020-06-17 22:57:47 +0900

    exercise

--- app/src/main/java/jp/ac/titech/itpro/sdl/map/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/map/MainActivity.java
@@ -1,8 +1,10 @@
 package jp.ac.titech.itpro.sdl.map;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -10,6 +12,9 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +30,8 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
@@ -51,12 +58,13 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
     private State state = State.STOPPED;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
+
+        Button Button = findViewById(R.id.button);
 
         infoView = findViewById(R.id.info_view);
         SupportMapFragment fragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
@@ -73,9 +81,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         locationClient = LocationServices.getFusedLocationProviderClient(this);
 
+        Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                locationClient.requestLocationUpdates(request, callback, null);
+            }
+        });
+
         request = new LocationRequest();
-        request.setInterval(10000L);
-        request.setFastestInterval(5000L);
+        //request.setInterval(10000L);
+        //request.setFastestInterval(5000L);
         request.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
 
         callback = new LocationCallback() {
